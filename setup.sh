d5c38503f1bb12dc7ce16241bf16415ec4eb107e
Ty Overby <ty@pre-alpha.com>
2022-01-02 01:21:29 -0500

    _

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+opam switch create ./ ocaml-base-compiler.4.12.1
+opam repo add janestreet-bleeding https://ocaml.janestreet.com/opam-repository
+opam repo add janestreet-bleeding-external https://github.com/janestreet/opam-repository.git#external-packages
+opam update
+opam install       \
+  base             \
+  dune             \
+  core             \
+  merlin           \
+  ppx_jane         \
+  incr_map         \
+  ocamlformat      \
+  incremental      \
+  incr_select      \
+  ocaml-lsp-server \
+  expect_test_helpers_core
+
+
